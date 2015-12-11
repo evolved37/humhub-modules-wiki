c6a080cd706f1cc5ad23b25a518eb2e7cabf24fd
evolved37 <evolved37@gmail.com>
2015-12-11 01:14:59 +0100

    Update views_page_edit.php

--- messages/it/views_page_edit.php
+++ messages/it/views_page_edit.php
@@ -1,9 +1,9 @@
 <?php
 return array (
   '<strong>Create</strong> new page' => '<strong>Crea</strong> una nuova pagina',
-  '<strong>Edit</strong> page' => '<strong>Modifica</strong> questa pagina',
-  'Enter a wiki page name or url (e.g. http://example.com)' => '',
-  'New page title' => '',
-  'Page content' => '',
+  '<strong>Edit</strong> page' => '<strong>Modifica</strong> pagina',
+  'Enter a wiki page name or url (e.g. http://example.com)' => 'Inserisci una pagina wiki o un url (e.s. http://example.com)',
+  'New page title' => 'Nuovo titolo di pagina',
+  'Page content' => 'Contenuto pagina',
   'Save' => 'Salva',
 );
